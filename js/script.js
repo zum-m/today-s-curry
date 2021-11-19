5ecf963822d8f314fb61eaf3ffc9e3efb17b9c72
zum-m <89333894+zum-m@users.noreply.github.com>
2021-11-19 09:55:56 +0900

    first commit

---
+++ js/script.js
@@ -0,0 +1,81 @@
+/* 
+Inspiration: http://drbl.in/oNQy
+*/
+
+
+
+
+$('#btn').on('click', function(){
+
+  const result=Math.ceil(Math.random()*3);
+  if(result===1){
+      $('#result').html('<img src="https://www.ebarafoods.com/recipe/detail/assets_img/b2bd9680-1a4b-11ea-b772-0b0d692d75ef/02803730-5ac5-11eb-bc15-9dfababa9991/02973_001.jpg">');
+  }else if(result===2){
+      $('#result').html('<img src="https://www.ebarafoods.com/recipe/detail/assets_img/75fdd3d0-1a4b-11ea-b146-eb8533844b42/2219f180-222b-11eb-ac9d-219969933cbe/00205_001.jpg">');
+  }else{
+      $('#result').html('<img src="https://www.ebarafoods.com/recipe/detail/assets_img/94d0b120-1a4b-11ea-a8c5-d96d16a09571/6ae36330-5abf-11eb-8968-b3ec670d9c67/01582_001.jpg">');
+  }
+  
+
+});
+
+//
+$('#btn2').on('click', function(){
+
+  const result=Math.ceil(Math.random()*3);
+  if(result===1){
+      $('#result').html('<img src="https://www.ebarafoods.com/recipe/detail/assets_img/a5eea5c0-1a4b-11ea-90c6-971cf413ddbb/1e82bf60-4039-11eb-bae3-47167fdf5ec8/02366_001.jpg">');
+  }else if(result===2){
+      $('#result').html('<img src="https://www.ebarafoods.com/recipe/detail/assets_img/9a89cf00-1a4b-11ea-b6dc-79c390e9c460/f58ab6f0-8528-11eb-8bcb-390d3991579b/01845_001.jpg">');
+  }else{
+      $('#result').html('<img src="https://www.ebarafoods.com/recipe/detail/assets_img/9dad9960-1a4b-11ea-8e7d-ad7bca582714/f96bb650-8529-11eb-a35d-a7d93f11c925/01989_001.jpg">');
+  }
+  
+
+});
+
+///
+$('#btn3').on('click', function(){
+
+  const result=Math.ceil(Math.random()*3);
+  if(result===1){
+      $('#result').html('<img src="https://www.ebarafoods.com/recipe/detail/assets_img/7cf193d0-1a4b-11ea-b49d-45e0766a21e1/1e4843b0-5ab8-11eb-8837-b9b638f6ad58/00204_001.jpg">');
+  }else if(result===2){
+      $('#result').html('<img src="https://www.ebarafoods.com/recipe/detail/assets_img/adfc2c70-1a4b-11ea-9d32-33f1fac3ef46/a54f7050-5ab8-11eb-b988-b5bef3e32bd1/00206_001.jpg">');
+  }else{
+      $('#result').html('<img src="https://www.ebarafoods.com/recipe/detail/assets_img/75b3c9c0-1a4b-11ea-9049-396b997324e2/cf416fc0-5ab8-11eb-a010-9d337a001abe/00207_001.jpg">');
+  }
+  
+
+});
+
+////
+$('#btn4').on('click', function(){
+
+  const result=Math.ceil(Math.random()*3);
+  if(result===1){
+      $('#result').html('<img src="https://www.ebarafoods.com/recipe/detail/assets_img/7a5af030-1a4b-11ea-a4c9-d938ab3c7ec4/ef641180-3476-11eb-8d39-cb3bb2a7411d/00208_001.jpg">');
+  }else if(result===2){
+      $('#result').html('<img src="https://www.ebarafoods.com/recipe/detail/assets_img/87c73370-1a4b-11ea-9283-b55dc668a8ac/fdc76300-5ab8-11eb-8575-dd76afbe40f2/00209_001.jpg">');
+  }else{
+      $('#result').html('<img src="https://www.ebarafoods.com/recipe/detail/assets_img/82714720-1a4b-11ea-8350-bbae62aa766c/9f98aaa0-5aba-11eb-bd92-17b1e072bc7f/00210_001.jpg">');
+  }
+  
+
+});
+
+
+/////
+$('#btn5').on('click', function(){
+
+  const result=Math.ceil(Math.random()*3);
+  if(result===1){
+      $('#result').html('<img src="https://www.ebarafoods.com/recipe/detail/assets_img/a18000c0-1a4b-11ea-9bed-c72693122793/d74a3110-3509-11eb-aa4d-c96be7ce5e0f/02165_001.jpg">');
+  }else if(result===2){
+      $('#result').html('<img src="https://www.ebarafoods.com/recipe/detail/assets_img/7b75faf0-1a4b-11ea-92c9-d98b03ac6680/c7921000-5aba-11eb-b910-370ce70aa57a/00217_001.jpg">');
+  }else{
+      $('#result').html('<img src="https://www.ebarafoods.com/recipe/detail/assets_img/7d02bb50-1a4b-11ea-950b-0b7c3e9b315f/f5479d80-5aba-11eb-927b-cbc9aaf85dd1/00218_001.jpg">');
+  }
+  
+
+});
